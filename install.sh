76954ebb3bc053df8d44596c00eac7025849f16b
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-13 11:21:36 +0100

    added buildpacks to install

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 git remote add staging git@heroku.com:jam2staging.git
+heroku config:add BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-php --app jam2staging
+
 git remote add production git@heroku.com:jam2production.git
+heroku config:add BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-php --app jam2production
 
 sudo npm install
 bower install
