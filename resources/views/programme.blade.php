08c1af7fcd1b4c8eb5d7d26e4b82b39457f3b918
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-14 08:59:30 +0100

    programme edits

--- resources/views/programme.blade.php
+++ resources/views/programme.blade.php
@@ -13,15 +13,22 @@
         <section class="container-narrow">
             <article>
                 <aside class="col-sm-3 time">
-                    <strong>9:00</strong>
-                    <div class="avatar"></div>
-                    <a href="" class="logo">Company Logo</a>
+                    <strong>9:00 am</strong>
+                    <img src="/images/speakers/scott-weiss.jpg">
+                    <a href="/" target="_blank" id="swiftkey"
+                       class="company"><em>SwiftKey</em></a>
                 </aside>
                 <aside class="col-sm-9 info">
                     <h3>Organizing Product Teams for Success</h3>
-                    <p class="speaker-name">Speaker Name</p>
-                    <p class="speaker-company">Speaker Company</p>
-                    <p>Lorem Ipsum.</p>
+
+                    <p class="speaker-name">Scott Weiss</p>
+
+                    <p class="speaker-company">SwiftKey</p>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting remaining essentially unchanged.</p>
                 </aside>
             </article>
         </section>
