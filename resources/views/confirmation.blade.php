9f2dca4b00cff0f7ffc553dc64d6e9dd069b1232
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-08-12 15:08:57 +0100

    added twitter tracking pixel to confirmation page

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -70,4 +70,16 @@
             </div>
         </section>
     </main>
+
+    <script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
+    <script type="text/javascript">twttr.conversion.trackPid('l6lck', {
+            tw_sale_amount: 0,
+            tw_order_quantity: 0
+        });</script>
+    <noscript>
+        <img height="1" width="1" style="display:none;" alt=""
+             src="https://analytics.twitter.com/i/adsct?txn_id=l6lck&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0"/>
+        <img height="1" width="1" style="display:none;" alt=""
+             src="//t.co/i/adsct?txn_id=l6lck&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0"/>
+    </noscript>
 @endsection
