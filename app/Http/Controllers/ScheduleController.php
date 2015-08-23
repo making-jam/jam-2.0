2e86ed6131b575f0c9342aea6d62f9865628f8d0
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-08-23 08:58:28 +0100

    added extra schedule items

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -9,7 +9,7 @@ class ScheduleController extends Controller
 {
     public function renderView()
     {
-        return view('schedule', ['title' => 'Schedule', 'bodyClass' => 'schedule', 'speakers' => self::scheduleData(),
+        return view('schedule', ['title' => 'Schedule', 'bodyClass' => 'schedule', 'schedule' => self::scheduleData(),
             'url' => parent::getMetaUrl()]);
     }
 
@@ -29,8 +29,40 @@ class ScheduleController extends Controller
             $data[$contentTimestamp] = $info;
         }
 
+        foreach (self::generateNonSpeakerSchedule() as $time => $title) {
+            $data[strtotime($time)] = ['programme' => [
+                'time' => $time,
+                'topic' => $title
+            ]];
+        }
+
         ksort($data);
 
         return $data;
+    }
+
+    private static function generateNonSpeakerSchedule()
+    {
+        $additionalSchedule = [
+            '9:00 am' => 'Checkin + coffee',
+            '9:30 am' => 'Welcome Speech',
+            '10:30 am' => 'QA',
+            '10:45 am' => 'Sponsors Intro',
+            '10:50 am' => 'Coffee Break',
+            '12:00 pm' => 'QA',
+            '12:15 pm' => 'Sponsors Intro',
+            '12:20 pm' => 'Lunch',
+            '2:00 pm' => 'QA',
+            '2:15 pm' => 'Sponsors Intro',
+            '2:20 pm' => 'Coffee Break',
+            '3:30 pm' => 'QA',
+            '3:45 pm' => 'Competition Givaways',
+            '3:55 pm' => 'Coffe Break',
+            '5:45 pm' => 'QA',
+            '6:00 pm' => 'Closing Words',
+            '6:15 pm' => 'Drinks'
+        ];
+
+        return $additionalSchedule;
     }
 }
--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -11,35 +11,43 @@
 
     <main>
         <section class="container-narrow">
-            @foreach ($speakers as $name => $info)
+            @foreach ($schedule as $timestamp => $info)
                 <article>
-                    <aside class="col-sm-2 time">
-                        <strong>{{$info['programme']['time']}}</strong>
-                    </aside>
-                    <aside class="col-sm-3 speaker">
-                        <img src="/images/speakers/optimised/{{$info['img']}}">
-                        <a href="{{$info['link']}}" target="_blank" id="{{$info['id']}}"
-                           class="company"><em>{{$info['company']}}</em></a>
-                    </aside>
-                    <aside class="col-sm-7 info">
-                        <h3>{{ $info['programme']['topic'] or 'Coming Soon...' }}</h3>
+                    @if (isset($info['biog']))
+                        <aside class="col-sm-2 time speaker-slot">
+                            <strong>{{$info['programme']['time']}}</strong>
+                        </aside>
+                        <aside class="col-sm-3 speaker">
+                            <img src="/images/speakers/optimised/{{$info['img']}}">
+                            <a href="{{$info['link']}}" target="_blank" id="{{$info['id']}}"
+                               class="company"><em>{{$info['company']}}</em></a>
+                        </aside>
+                        <aside class="col-sm-7 info">
+                            <h3>{{ $info['programme']['topic'] or 'Coming Soon...' }}</h3>
 
-                        <p>{{$info['name']}}</p>
+                            <p>{{$info['name']}}</p>
 
-                        <p class="speaker-company">{{$info['position']}}, {{$info['company']}}</p>
+                            <p class="speaker-company">{{$info['position']}}, {{$info['company']}}</p>
 
-                        {!! $info['topic']['description'] !!}
+                            {!! $info['topic']['description'] !!}
 
+                            <div class="about">
+                                <h4>About {{$info['firstName']}}</h4>
 
-                        <div class="about">
-                            {{--<span class="label">Topic</span>--}}
-                            <h4>About {{$info['firstName']}}</h4>
-
-                            <div class="description">
-                                {!! $info['biog'] !!}
+                                <div class="description">
+                                    {!! $info['biog'] !!}
+                                </div>
                             </div>
-                        </div>
-                    </aside>
+                        </aside>
+                    @else
+                        <aside class="col-sm-2 time no-speaker-slot">
+                            <strong>{{$info['programme']['time']}}</strong>
+                        </aside>
+                        <aside class="col-sm-3 speaker"></aside>
+                        <aside class="col-sm-7 info">
+                            <h3>{{ $info['programme']['topic'] }}</h3>
+                        </aside>
+                    @endif
                 </article>
             @endforeach
         </section>
