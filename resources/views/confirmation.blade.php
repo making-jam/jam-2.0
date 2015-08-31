3c90d1cc1f18c820df674e0c96f7d5c7dd00e746
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-08-31 07:17:19 +0100

    added facebook conversion tracking code

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -82,4 +82,19 @@
         <img height="1" width="1" style="display:none;" alt=""
              src="//t.co/i/adsct?txn_id=l6lck&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0"/>
     </noscript>
+
+    <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '409138232625910');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="https://www.facebook.com/tr?id=409138232625910&ev=PageView&noscript=1"/>
+    </noscript>
 @endsection
